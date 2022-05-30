814837901ca473d0d5634476e006b93310b88f1c
Ratul Buragohain <ratul75@hotmail.com>
2022-05-30 20:35:02 +0000

    Clean up

--- longest_common_prefix/src/lib.rs
+++ longest_common_prefix/src/lib.rs
@@ -1,50 +1,41 @@
-///Find the longest common prefix among array of strings
+/***
+ * Find the longest prefix from an array of strings
+ ***/
 
-pub fn longest_common_prefix(strings: &[String]) -> String {
-    if strings.is_empty() {
+pub fn longest_common_prefix(arr: &[&str]) -> String {
+    if arr.len() == 0 {
         return String::new();
     }
-    let mut longest_common_prefix = String::new();
-    let mut char_vecs = Vec::<Vec<char>>::with_capacity(strings.len());
-    strings
-        .iter()
-        .for_each(|string| char_vecs.push(string.chars().collect()));
+    let mut result = String::new();
+    let first: Vec<char> = arr[0].chars().collect();
+    let rests: Vec<Vec<char>> = arr[1..].iter().map(|s| s.chars().collect()).collect();
     let mut i = 0;
-    while i < strings[0].len() {
-        let ch = char_vecs[0][i];
-        for j in 1..strings.len() {
-            if i < strings[j].len() && ch == char_vecs[j][i] {
+
+    while i < first.len() {
+        let ch = first[i];
+        for j in 0..rests.len() {
+            if i < rests[j].len() && ch == rests[j][i] {
                 continue;
             } else {
-                return longest_common_prefix;
+                return result;
             }
         }
-        longest_common_prefix.push(ch);
+        result.push(ch);
         i += 1;
     }
-    longest_common_prefix
+
+    result
 }
 
 #[cfg(test)]
 mod tests {
-    use super::longest_common_prefix;
+    use super::*;
     #[test]
-    fn test_longest_common_prefix() {
-        assert_eq!(
-            longest_common_prefix(&[
-                String::from("flower"),
-                String::from("flow"),
-                String::from("flight")
-            ]),
-            String::from("fl")
-        );
-        assert_eq!(
-            longest_common_prefix(&[
-                String::from("flower"),
-                String::from("flower"),
-                String::from("flower")
-            ]),
-            String::from("flower")
-        );
+    fn longest_common_prefix_test_1() {
+        let arr = ["flower", "flow", "flight"];
+        assert_eq!(longest_common_prefix(&arr), String::from("fl"));
+
+        let arr = ["flower1", "flower2", "flower3"];
+        assert_eq!(longest_common_prefix(&arr), String::from("flower"));
     }
 }
