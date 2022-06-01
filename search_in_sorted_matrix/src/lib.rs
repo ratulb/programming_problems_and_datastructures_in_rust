984d5624715c13e291eeba8a9c7c1e334383f349
Ratul Buragohain <ratul75@hotmail.com>
2022-06-01 09:22:02 +0000

    Method signature

--- search_in_sorted_matrix/src/lib.rs
+++ search_in_sorted_matrix/src/lib.rs
@@ -1,35 +1,38 @@
-pub fn search(matrix: &[&[i32]], key: i32) -> (i32, i32) {
+#![allow(unused_comparisons)]
+/***
+ * Search a key in a matrix where row as well as colum values are sorted in ascending order
+ ***/
+pub fn search(matrix: &[&[i32]], key: i32) -> Option<(usize, usize)> {
     let rows = matrix.len();
     let cols = matrix[0].len();
-    let mut i: usize = 0;
-    let not_found = (-1, -1);
-    let mut j: usize = cols - 1;
-    while (i >= 0 && i < rows) && (j >= 0 && j < cols) {
-        if matrix[i][j] == key {
-            return (i as i32, j as i32);
-        } else if matrix[i][j] > key {
-            if j > 0 {
-                j -= 1;
+    let mut row: usize = 0;
+    let mut col: usize = cols - 1;
+    while (row >= 0 && row < rows) && (col >= 0 && col < cols) {
+        if matrix[row][col] == key {
+            return Some((row, col));
+        } else if matrix[row][col] > key {
+            if col > 0 {
+                col -= 1;
             } else {
-                return not_found;
+                return None;
             }
-        } else if matrix[i][j] < key {
-            i += 1;
+        } else if matrix[row][col] < key {
+            row += 1;
         }
     }
-    return not_found;
+    return None;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::search;
     #[test]
     fn test_search() {
         let row1 = [12, 34, 45, 67];
         let row2 = [14, 35, 47, 78];
         let row3 = [19, 37, 51, 81];
         let matrix = [&row1[..], &row2[..], &row3[..]];
-        assert_eq!(search(&matrix[..], 51), (2, 2));
-        assert_eq!(search(&matrix[..], 510), (-1, -1));
+        assert_eq!(search(&matrix[..], 51), Some((2, 2)));
+        assert_eq!(search(&matrix[..], 510), None);
     }
 }
