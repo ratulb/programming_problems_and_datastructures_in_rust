511645c8cc7a83dd3bbc018fdae8dca88aa162b1
Ratul <ratul75@hotmail.com>
2022-06-03 16:09:53 +0530

    Update lib.rs

--- longest_common_suffix/src/lib.rs
+++ longest_common_suffix/src/lib.rs
@@ -6,7 +6,13 @@ pub fn longest_common_suffix(arr: &[&str]) -> String {
     if arr.len() == 0 {
         return String::new();
     }
-    let mut result = String::new();
+    //Suffix would be as long as the min length of all the strings
+    let mut result = String::with_capacity(
+        arr.iter()
+            .min_by(|p, n| p.len().cmp(&n.len()))
+            .unwrap()
+            .len(),
+    );
     let mut first = arr[0].chars();
     let mut rests: Vec<_> = arr[1..].iter().map(|s| s.chars()).collect();
     while let Some(ch) = first.next_back() {
