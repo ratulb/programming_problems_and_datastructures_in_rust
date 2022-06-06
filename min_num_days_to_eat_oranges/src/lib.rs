f4044695867d864cef0273ec79d82ff8a385f4fc
Ratul <ratul75@hotmail.com>
2022-06-06 15:41:50 +0530

    Update lib.rs

--- min_num_days_to_eat_oranges/src/lib.rs
+++ min_num_days_to_eat_oranges/src/lib.rs
@@ -4,7 +4,8 @@ every day as follows:
 
 Eat one orange.
 If the number of remaining oranges n is divisible by 2 then you can eat n / 2 oranges.
-If the number of remaining oranges n is divisible by 3 then you can eat 2 * (n / 3) oranges.
+If the number of remaining oranges n is divisible by 3 then you can eat 2 * (n / 3)
+oranges.
 You can only choose one of the actions per day.
 ***/
 
