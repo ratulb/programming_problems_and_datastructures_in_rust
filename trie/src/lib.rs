1728833127cc4d57a6956cb069b9de8294c1c5a5
Ratul Buragohain <ratul75@hotmail.com>
2022-06-07 07:23:12 +0000

    Trie data structure

---
+++ trie/src/lib.rs
@@ -0,0 +1,72 @@
+/*** Implement Trie data structure that holds string ***/
+
+use std::collections::HashMap;
+struct Node {
+    children: HashMap<char, Node>,
+    is_word: bool,
+}
+
+impl Node {
+    fn new() -> Self {
+        Node {
+            children: HashMap::new(),
+            is_word: false,
+        }
+    }
+}
+pub struct Trie {
+    root: Node,
+}
+
+impl Trie {
+    pub fn new() -> Self {
+        Trie { root: Node::new() }
+    }
+
+    pub fn insert(&mut self, word: &str) {
+        let mut current = &mut self.root;
+        let mut chars = word.chars();
+        while let Some(c) = chars.next() {
+            current = current.children.entry(c).or_insert(Node::new());
+        }
+        current.is_word = true;
+    }
+
+    pub fn search(&self, word: &str) -> bool {
+        let mut current = &self.root;
+        let mut chars = word.chars();
+        while let Some(c) = chars.next() {
+            match current.children.get(&c) {
+                Some(ref node) => current = node,
+                None => return false,
+            }
+        }
+        current.is_word
+    }
+
+    pub fn starts_with(&self, prefix: &str) -> bool {
+        let mut current = &self.root;
+        let mut chars = prefix.chars();
+        while let Some(c) = chars.next() {
+            match current.children.get(&c) {
+                Some(ref node) => current = node,
+                None => return false,
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Trie;
+    #[test]
+    fn test_trie() {
+        let mut trie = Trie::new();
+        trie.insert("hello");
+        assert!(trie.search("hello"));
+        assert!(!trie.search("world"));
+        assert!(trie.starts_with("hell"));
+        assert!(!trie.starts_with("helo"));
+    }
+}
