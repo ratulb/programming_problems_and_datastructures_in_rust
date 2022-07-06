4ffc96c4fc0126e9b48f220a165969385d62b3ed
Ratul Buragohain <ratul75@hotmail.com>
2022-07-06 15:04:43 +0000

    Docs

--- longest_common_prefix/src/lib.rs
+++ longest_common_prefix/src/lib.rs
@@ -13,16 +13,23 @@ pub fn longest_common_prefix(arr: &[&str]) -> String {
             .unwrap()
             .len(),
     );
+    //Get the characters of the first string in the in an iterator
     let mut first = arr[0].chars();
+    //Map rests of the strings to iterator of chars and hold them in a vector
     let mut rests: Vec<_> = arr[1..].iter().map(|s| s.chars()).collect();
+    //Iterate through the characters of the first string
     while let Some(ch) = first.next() {
         for i in 0..rests.len() {
             let current = &mut rests[i];
             match current.next() {
+                //Next string's next char matches with the first string char
+                //jump to the next string
                 Some(c) if c == ch => continue,
+                //Does not match - return whatever we have go so far
                 _ => return result,
             }
         }
+        //All strings next char matched, lengthen result
         result.push(ch);
     }
     result
