6a5a98f3aac9b7de35861f81825d56cf493735f5
Ratul Buragohain <ratul75@hotmail.com>
2022-07-24 00:05:36 +0000

    Quick sort

--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -1,48 +1,63 @@
-/**
- * Sample implementation of quick sort alogrithm in rust for u16 type
- */
+///Implementation of Quick Sort algorithm
+use rand::Rng;
 
-pub fn quicksort<'a>(array: &'a mut [u16]) -> &'a mut [u16] {
-    sort(array, 0, array.len() - 1);
-    array
+pub fn quicksort<T: PartialOrd>(array: &mut [T]) {
+    let mut rng = rand::thread_rng();
+    sort(array, 0, array.len() - 1, &mut rng);
 }
-fn sort(array: &mut [u16], start: usize, end: usize) {
-    if start < end {
-        let partition = partition(array, start, end);
+//Uses the right most element as pivot
+fn right_as_pivot<T: PartialOrd>(array: &mut [T], left: usize, right: usize) -> usize {
+    let mut i = left;
+    for j in left..right {
+        if array[j] <= array[right] {
+            array.swap(i, j);
+            i += 1;
+        }
+    }
+    array.swap(i, right);
+    i
+}
+//Uses left most element as the as pivot
+fn left_as_pivot<T: PartialOrd>(array: &mut [T], left: usize, right: usize) -> usize {
+    let mut i = right;
+    for j in (left + 1..=right).rev() {
+        if array[j] > array[left] {
+            array.swap(i, j);
+            i -= 1;
+        }
+    }
+    array.swap(i, left);
+    i
+}
+
+//Recursive parttion sort algorithm
+
+fn sort<T: PartialOrd>(array: &mut [T], left: usize, right: usize, random: &mut impl Rng) {
+    if left < right {
+        let partition = partition(array, left, right, random);
         if array[..partition].len() > 1 {
-            sort(array, start, partition - 1);
+            sort(array, left, partition - 1, random);
         }
         if array[partition + 1..].len() > 1 {
-            sort(array, partition + 1, end);
+            sort(array, partition + 1, right, random);
         }
     }
 }
-fn partition(array: &mut [u16], left: usize, right: usize) -> usize {
-    let pivot = array[right];
-    let mut left_ptr = left;
-    let mut right_ptr = right;
 
-    loop {
-        while left_ptr < right_ptr && array[left_ptr] <= pivot {
-            left_ptr += 1;
-        }
-        right_ptr -= 1;
-        while right_ptr > left_ptr && array[right_ptr] > pivot {
-            right_ptr -= 1;
-        }
-        if left_ptr >= right_ptr {
-            break;
-        }
-        swap(array, left_ptr, right_ptr);
+///Parttion the array segments using either left or right most elements as
+///the pivots randomly.
+
+fn partition<T: PartialOrd>(
+    array: &mut [T],
+    left: usize,
+    right: usize,
+    random: &mut impl Rng,
+) -> usize {
+    if random.gen::<bool>() {
+        left_as_pivot(array, left, right)
+    } else {
+        right_as_pivot(array, left, right)
     }
-    swap(array, left_ptr, right);
-    left_ptr
-}
-
-fn swap(array: &mut [u16], left_ptr: usize, right_ptr: usize) {
-    let temp = array[left_ptr];
-    array[left_ptr] = array[right_ptr];
-    array[right_ptr] = temp;
 }
 
 #[cfg(test)]
@@ -51,7 +66,7 @@ mod tests {
     use rand::Rng;
     #[test]
     fn quicksort_test() {
-        let mut runs = 5;
+        let mut runs = 50000;
         loop {
             let mut array: [u16; 20] = [0; 20];
             rand::thread_rng().fill(&mut array);
@@ -59,7 +74,6 @@ mod tests {
             if !is_sorted(&array) {
                 panic!("Array is not sorted...");
             }
-            println!("The sorted array is : {:?} ", array);
             runs -= 1;
             if runs == 0 {
                 break;
