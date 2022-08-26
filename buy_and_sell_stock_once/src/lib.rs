6533089c45154932d50764d9ce65800c98955990
Ratul Buragohain <ratul75@hotmail.com>
2022-08-26 18:32:40 +0000

    Changes

--- buy_and_sell_stock_once/src/lib.rs
+++ buy_and_sell_stock_once/src/lib.rs
@@ -1,14 +1,5 @@
-/***
-You are given an array prices where prices[i] is the price of a given stock
-on the ith day.
-
-You want to maximize your profit by choosing a single day to buy one stock
-and choosing a different day in the future to sell that stock.
-
-Return the maximum profit you can achieve from this transaction. If you cannot
-achieve any profit, return 0.
-***/
-
+//Buy and sell stock once - max profit
+use std::cmp::max;
 pub fn max_profit(prices: Vec<i32>) -> i32 {
     let mut max_profit = 0;
     let mut left = 0; //Buy stock on day 'left'
@@ -16,7 +7,7 @@ pub fn max_profit(prices: Vec<i32>) -> i32 {
 
     while right < prices.len() {
         if prices[left] < prices[right] {
-            max_profit = std::cmp::max(max_profit, prices[right] - prices[left]);
+            max_profit = max(max_profit, prices[right] - prices[left]);
         } else {
             left = right;
         }
