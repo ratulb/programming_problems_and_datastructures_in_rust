1c6561876ed632a79f5523465c20215f068f131c
Ratul Buragohain <ratul75@hotmail.com>
2022-08-26 10:53:14 +0000

    Modifications

--- pascal_triangle/src/lib.rs
+++ pascal_triangle/src/lib.rs
@@ -2,46 +2,72 @@
  * Generate Pascal's triangle
  */
 
-pub fn generate(layers: usize) -> Vec<Vec<u32>> {
-    if layers == 0 {
+pub fn generate(rows: usize) -> Vec<Vec<usize>> {
+    if rows == 0 {
         return vec![];
     }
-    let mut triangle = Vec::with_capacity(layers);
-    triangle.push(vec![]);
-    triangle[0].push(1);
-    for i in 1..layers {
-        let prev_layer = &triangle[i - 1];
-        let mut curr_layer: Vec<u32> = vec![1];
+    let mut triangle = Vec::with_capacity(rows);
+    triangle.push(vec![1]);
+    for i in 1..rows {
+        let prev_row = &triangle[i - 1];
+        let mut curr_row: Vec<_> = vec![1];
         for j in 1..i {
-            curr_layer.push(prev_layer[j - 1] + prev_layer[j]);
+            curr_row.push(prev_row[j - 1] + prev_row[j]);
         }
-        curr_layer.push(1);
-        triangle.push(curr_layer);
+        curr_row.push(1);
+        triangle.push(curr_row);
     }
     triangle
+}
+
+pub fn nth(row: usize) -> Vec<usize> {
+    match row {
+        n if n < 1 => vec![],
+        n if n == 1 => vec![1],
+        n => {
+            let prev_row = nth(n - 1);
+            let mut curr = Vec::with_capacity(n);
+            curr.push(1);
+            for i in 1..prev_row.len() {
+                curr.push(prev_row[i - 1] + prev_row[i]);
+            }
+            curr.push(1);
+            curr
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_no_layer() {
-        assert_eq!(generate(0), Vec::<Vec<u32>>::new());
+    fn test_no_row() {
+        assert_eq!(generate(0), Vec::<Vec<_>>::new());
     }
     #[test]
-    fn test_one_layer() {
-        let layer1 = vec![1];
-        let triangle = vec![layer1];
+    fn test_one_row() {
+        let row1 = vec![1];
+        let triangle = vec![row1];
         assert_eq!(generate(1), triangle);
     }
     #[test]
-    fn test_multi_layer() {
-        let layer1 = vec![1];
-        let layer2 = vec![1, 1];
-        let layer3 = vec![1, 2, 1];
-        let layer4 = vec![1, 3, 3, 1];
-        let layer5 = vec![1, 4, 6, 4, 1];
-        let triangle = vec![layer1, layer2, layer3, layer4, layer5];
+    fn test_multi_rows() {
+        let row1 = vec![1];
+        let row2 = vec![1, 1];
+        let row3 = vec![1, 2, 1];
+        let row4 = vec![1, 3, 3, 1];
+        let row5 = vec![1, 4, 6, 4, 1];
+        let triangle = vec![row1, row2, row3, row4, row5];
         assert_eq!(generate(5), triangle);
+    }
+
+    #[test]
+    fn test_nths() {
+        assert_eq!(nth(0), vec![]);
+        assert_eq!(nth(1), vec![1]);
+        assert_eq!(nth(2), vec![1, 1]);
+        assert_eq!(nth(3), vec![1, 2, 1]);
+        assert_eq!(nth(4), vec![1, 3, 3, 1]);
+        assert_eq!(nth(5), vec![1, 4, 6, 4, 1]);
     }
 }
